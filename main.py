a54bff526deb71b56d9186f07a8c6ef048656109
veebch <mart@veeb.ch>
2022-12-10 23:25:38 +0100

    added crude schedule

--- main.py
+++ main.py
@@ -33,13 +33,13 @@ def demo(np):
 def bar(np, upto):
     for i in range(upto):
         if i<=upto:
-            np[i]= (5, 0 ,60)
+            np[i]= (255, 50 ,25)
     np.write()
     
 def init(np, upto):
     for i in range(upto):
         if i<=upto:
-            np[i]= (5, 0 , 60)
+            np[i]= (255, 50 ,25)
             utime.sleep_ms(20)
             np.write()
             
@@ -53,20 +53,23 @@ def off(np):
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
 wlan.connect(secrets.SSID, secrets.PASSWORD)
-print(wlan.isconnected())
+while wlan.isconnected()!= True:
+    utime.sleep(1)
 n = 144
 p = 15
-rtc = machine.RTC()
-print(rtc.datetime())
 np = neopixel.NeoPixel(machine.Pin(p), n)
-
-upto=int(math.floor(n*(float(rtc.datetime()[4])+float(rtc.datetime()[4])/60)/24))
 try:
+    rtc = machine.RTC() 
+    upto=int(math.floor(n*(float(rtc.datetime()[4])+float(rtc.datetime()[4])/60)/24))
     demo (np)
     init (np, upto)
+
     while True:
         bar(np, upto)
         utime.sleep(100)
 except:
     off(np)
+    
+    
+
     
