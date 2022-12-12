bb8eeb4e68e2969217c8532f9e7ef397a6c8241f
veebch <mart@veeb.ch>
2022-12-12 14:22:05 +0100

    added hometime (and startup) rainbow

--- main.py
+++ main.py
@@ -65,6 +65,36 @@ def eventnow(hoursin):
             meetingnow = True
     return meetingnow
 
+def wheel(pos):
+    # Input a value 0 to 255 to get a color value.
+    # The colours are a transition r - g - b - back to r.
+    if pos < 0 or pos > 255:
+        r = g = b = 0
+    elif pos < 85:
+        r = int(pos * 3)
+        g = int(255 - pos * 3)
+        b = 0
+    elif pos < 170:
+        pos -= 85
+        r = int(255 - pos * 3)
+        g = 0
+        b = int(pos * 3)
+    else:
+        pos -= 170
+        r = 0
+        g = int(pos * 3)
+        b = int(255 - pos * 3)
+    return (r, g, b) 
+
+
+def rainbow_cycle(wait):
+    for j in range(255):
+        for i in range(n):
+            pixel_index = (i * 256 // n) + j
+            np[i] = wheel(pixel_index & 255)
+        np.write()
+        time.sleep(wait)
+
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
 wlan.connect(secrets.SSID, secrets.PASSWORD)
@@ -76,6 +106,8 @@ todayseventsurl=secrets.LANURL
 response=urequests.get(todayseventsurl).json()
 now = set_time()
 count = 1
+rainbow_cycle(0.001)  # rainbow cycle with 1ms delay per step
+off(np)
 firstrun = True   			# When you plug in, update rather than wait until the stroke of the next minute
 try:
     while True:
@@ -84,6 +116,8 @@ try:
             if hoursin >= clockin and hoursin <= clockout:
                 bar(np, hoursin)
                 addmeet(np,response)
+            elif hourtoindex(hoursin) == hourtoindex(clockout)+1:
+                rainbow_cycle(0.001)
             else:
                 off(np)
         if firstrun:		# If this was the initial update, mark it as complete
