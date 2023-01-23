ceb512faa1d2c5812c39a4ae03f87cd93027dcdc
veebch <mart@veeb.ch>
2023-01-23 14:53:05 +0100

    tidy

--- main.py
+++ main.py
@@ -14,14 +14,14 @@ import socket
 import struct
 
 NTP_DELTA = 2208988800 	# NTP delta is 1 Jan 1970 for host
-GMT_OFFSET = 3600      	# hack because timezone support seems to be lacking, seconds relative to GMT
+GMT_OFFSET = 3600      	# hack, seconds relative to GMT
 host = "pool.ntp.org"	# The ntp server used for grabbing time
 n = 144   				# Number of pixels on strip
 p = 15    				# GPIO pin that data line of lights is connected to
-barcolor = (0, 25 ,0)	# RGB for bar color
-eventcolor = (0,0,255)	# RGB for event color
-flip=False				# Flip display (set to True if the strip runs from right to left)
-googlecalbool=True		# Boolean for whether to check google calendar page
+barcolor = (0, 25, 0)	# RGB for bar color
+eventcolor = (0, 0, 255)	# RGB for event color
+flip = False				# Flip display (set to True if the strip runs from right to left)
+googlecalbool = True		# Boolean for whether to check google calendar page
 led = machine.Pin("LED", machine.Pin.OUT)
 led.off()
 led.on()
@@ -71,11 +71,13 @@ schedule = {
     ]
 }
 
+
 def whatday(weekday):
     dayindex = int(weekday)
     nameofday = ['monday', 'tuesday', 'wednesday', 'thursday','friday','saturday','sunday']
     day = nameofday[dayindex]
     return day
+
 
 def set_time():
     NTP_QUERY = bytearray(48)
@@ -94,6 +96,7 @@ def set_time():
     clock = machine.RTC().datetime((tm[0], tm[1], tm[2], tm[6] + 1, tm[3], tm[4], tm[5], 0  ))
     return tm[6]+1
 
+
 def bar(np, upto):
     barupto = hourtoindex(upto)
     for i in range(n):
@@ -107,24 +110,29 @@ def bar(np, upto):
                 np[i] = barcolor
             else:
                 np[i] = (0,0,0)
-  
+
+
+
 def addevents(np,response):
     for x in response:
         index = hourtoindex(x)
         if valid(index):
             np[index] = eventcolor
 
+
 def valid(index):
     valid=False
     if index <= n and index > 0:
         valid = True
     return valid
-                
+
+
 def off(np):
     for i in range(n):
         np[i]= (0, 0 , 0)
         np.write()
-        
+
+
 def hourtoindex(hoursin):
     index=int(math.floor(n*(float (hoursin) - clockin)/(clockout-clockin)))
     if flip ==  True:
@@ -133,12 +141,14 @@ def hourtoindex(hoursin):
         index = -1
     return index
 
+
 def eventnow(hoursin,response):
     event = False
     for x in response:
         if hourtoindex(x) == hourtoindex(hoursin):
             event = True
     return event
+
 
 def wheel(pos):
     # Input a value 0 to 255 to get a color value.
@@ -159,7 +169,7 @@ def wheel(pos):
         r = 0
         g = int(pos * 3)
         b = int(255 - pos * 3)
-    return (r, g, b) 
+    return (r, g, b)
 
 
 def rainbow_cycle(np):
@@ -168,7 +178,9 @@ def rainbow_cycle(np):
             pixel_index = (i * 256 // n) + j
             np[i] = wheel(pixel_index & 255)
         np.write()
-        
+
+
+
 def atwork(clockin,clockout,time):
     index = -1
     if clockin != clockout:
@@ -177,20 +189,22 @@ def atwork(clockin,clockout,time):
     if index > -1:
         work = True
     return work
-        
+
+
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
 wlan.connect(secrets.SSID, secrets.PASSWORD)
-while wlan.isconnected()!= True:
+while wlan.isconnected() is not True:
     time.sleep(1)
     print("Not connecting to WiFi\nWaiting\n")
 np = neopixel.NeoPixel(machine.Pin(p), n)
-todayseventsurl=secrets.LANURL
+todayseventsurl = secrets.LANURL
 count = 1
-firstrun = True   															# When you plug in, update rather than wait until the stroke of the next minute
+firstrun = True 
+# When you plug in, update rather than wait until the stroke of the next minute
 print("connected to WiFi: Start loop")
 off(np)
-shonetoday=True
+shonetoday = True
 led.off()
 set_time()
 while True:
@@ -204,39 +218,41 @@ while True:
         working = atwork(clockin, clockout, hoursin)
         print(working, hoursin)
         if working is True:
-            shonetoday=False
+            shonetoday = False
             # If not working, no lights will show
             # update lights at the stroke of every minute, or on first run
             bar(np, hoursin)
             if googlecalbool is True:
-                response=urequests.get(todayseventsurl).json()
-                eventbool = eventnow(hoursin,response)
-                addevents(np,response)
+                response = urequests.get(todayseventsurl).json()
+                eventbool = eventnow(hoursin, response)
+                addevents(np, response)
             else:
                 # This is where you would add hardcoded events if you were not using google
                 eventbool =  False
-            if firstrun:												# If this was the initial update, mark it as complete
+            if firstrun:
+                # If this was the initial update, mark it as complete
                 firstrun = False
-            count = (count + 1) % 2										# The value used to toggle lights
+            count = (count + 1) % 2	
+            # The value used to toggle lights
             if  eventbool == True:  									# If an event is starting, flash all LEDS otherwise just the end of the bar
                 for i in range(n):
                     np[i]=tuple(z*count for z in eventcolor) 			# All lights
             else:
-                ledindex = min(hourtoindex(hoursin),n)
-                np[ledindex]=tuple(z*count for z in barcolor) 			# Just the tip of the bar
+                ledindex = min(hourtoindex(hoursin), n)
+                np[ledindex] = tuple(z*count for z in barcolor) 			# Just the tip of the bar
             np.write()
         else:
             if shonetoday is False:
                 led.on()
                 rainbow_cycle(np)
-                shonetoday=True
+                shonetoday = True
                 off(np)
                 time.sleep(600)
-        if wlan.isconnected()!= True:
+        if wlan.isconnected() is not True:
             wlan = network.WLAN(network.STA_IF)
             wlan.active(True)
             wlan.connect(secrets.SSID, secrets.PASSWORD)
-            while wlan.isconnected()!= True:
+            while wlan.isconnected() is not True:
                 time.sleep(1)
                 print("Not connecting to WiFi\nWaiting\n")
         if now[5] == 0 and now[4] == 44 and now[3] == 4:
