a1bae7a06b6c55c66d754b716d8175e22b47d305
veebch <mart@veeb.ch>
2023-05-09 12:10:37 +0200

    moved more parameters to this file

--- config_example.py
+++ config_example.py
@@ -1,3 +1,10 @@
 SSID = "wifissid"
 PASSWORD = "trustnoone"
-LANURL = "host.local/array.json"
+CALENDAR = "INSERT YOUR CALENDAR NAME"
+APIKEY = "INSERT YOUR APIKEY"
+CLOCK = "https://timeapi.io/api/TimeZone/zone?timeZone=Europe/Zurich"
+# Lighting
+PIXELS = 144
+GPIOPIN = 15
+BARCOL = (0,100,0)
+EVENTCOL = (0, 0, 255)
