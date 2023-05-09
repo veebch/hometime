24940174c1d1e915a7974857f24d9755689f0d26
veebch <mart@veeb.ch>
2023-05-09 12:24:02 +0200

    moved more parameters to this file

--- config_example.py
+++ config_example.py
@@ -8,3 +8,49 @@ PIXELS = 144
 GPIOPIN = 15
 BARCOL = (0,100,0)
 EVENTCOL = (0, 0, 255)
+FLIP = False                # Flip display (set to True if the strip runs from right to left)
+GOOGLECALBOOL = True        # Boolean for whether to check google calendar page
+SCHEDULE = {
+    "monday": [
+      {
+        "clockin": "9",
+        "clockout": "17"
+      }
+    ],
+    "tuesday": [
+      {
+        "clockin": "9",
+        "clockout": "17"
+      }
+    ],
+    "wednesday": [
+      {
+        "clockin": "9",
+        "clockout": "17"
+      }
+    ],
+    "thursday": [
+      {
+        "clockin": "9",
+        "clockout": "17"
+      }
+    ],
+    "friday": [
+      {
+        "clockin": "9",
+        "clockout": "17"
+      }
+    ],
+    "saturday": [
+      {
+        "clockin": "0",
+        "clockout": "0"
+      }
+    ],
+    "sunday": [
+      {
+        "clockin": "0",
+        "clockout": "0"
+      }
+    ]
+}
